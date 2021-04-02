9a00b613732a0ac0191f022247a56e1563eacc06
arjundubey-cr <arjundubeycr@gmail.com>
2021-04-02 11:29:17 +0530

    corrected functions in space_age and made code look cleaner

--- space-age/space_age.cpp
+++ space-age/space_age.cpp
@@ -2,23 +2,16 @@
 
 namespace space_age
 {
-    int space_age::seconds() const
-    {
-        return age_in_seconds;
-    }
-    double space_age::on_earth() const
-    {
-        return (age_in_seconds / EARTH_YEAR_IN_SECS);
-    }
-    double space_age::on_mercury() const
-    {
-        return (on_earth() / 0.2408467);
-    }
-    double space_age::on_venus() const { return on_earth() / 0.61519726; }
-    double space_age::on_neptune() const { return on_earth() / 164.79132; }
-    double space_age::on_mars() const { return on_earth() / 1.8808158; }
-    double space_age::on_jupiter() const { return on_earth() / 11.862615; }
-    double space_age::on_saturn() const { return on_earth() / 29.447498; }
-    double space_age::on_uranus() const { return on_earth() / 84.016846; }
+    double space_age::calculate_age(std::string planet_name) const { return on_earth() / year_wrt_earth.at(planet_name); }
+    space_age::space_age(const long long int seconds) { age_in_seconds = seconds; }
+    long long int space_age::seconds() const { return age_in_seconds; }
+    double space_age::on_earth() const { return age_in_seconds / earth_year_in_seconds; }
+    double space_age::on_mercury() const { return calculate_age("Mercury"); }
+    double space_age::on_venus() const { return calculate_age("Venus"); }
+    double space_age::on_neptune() const { return calculate_age("Neptune"); }
+    double space_age::on_mars() const { return calculate_age("Mars"); }
+    double space_age::on_jupiter() const { return calculate_age("Jupiter"); }
+    double space_age::on_saturn() const { return calculate_age("Saturn"); }
+    double space_age::on_uranus() const { return calculate_age("Uranus"); }
 
 } // namespace space_age
--- space-age/space_age.h
+++ space-age/space_age.h
@@ -1,22 +1,31 @@
 #if !defined(SPACE_AGE_H)
 #define SPACE_AGE_H
 
+#include <string>
+#include <map>
+
 namespace space_age
 {
     class space_age
     {
 
-        double const EARTH_YEAR_IN_SECS = 31557600;
+        double const earth_year_in_seconds = 31557600;
+        std::map<std::string, double> year_wrt_earth =
+            {{"Mercury", 0.2408467},
+             {"Venus", 0.61519726},
+             {"Mars", 1.8808158},
+             {"Jupiter", 11.862615},
+             {"Saturn", 29.447498},
+             {"Uranus", 84.016846},
+             {"Neptune", 164.79132}};
 
     private:
         long long int age_in_seconds;
 
     public:
-        space_age(const long long int age)
-        {
-            age_in_seconds = age;
-        }
-        int seconds() const;
+        explicit space_age(const long long int seconds);
+        long long int seconds() const;
+        double calculate_age(std::string planet_name) const;
         double on_earth() const;
         double on_mars() const;
         double on_venus() const;
